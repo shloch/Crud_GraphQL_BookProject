68b0b9a0eff509a3d7a5d34296435899a1191b71
shloch <shloch2007@yahoo.fr>
2021-12-11 09:34:39 +0100

    setup initial schema

---
+++ server/schema/schema.js
@@ -0,0 +1,30 @@
+const grapjql = require('graphql')
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                //code to get data from DB:source
+            }
+        }
+    }
+})
+
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
