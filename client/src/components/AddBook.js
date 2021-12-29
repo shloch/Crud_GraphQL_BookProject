c4302370d180820c1f67881f70f764207325722a
shloch <shloch2007@yahoo.fr>
2021-12-30 00:42:56 +0100

    core functionality working

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,37 +1,61 @@
 import React from 'react'
-import { useQuery } from "@apollo/client";
-import getAuthorsQuery from './graphql/getAuthors'
+import { useQuery, useMutation } from "@apollo/client";
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../graphql/queries'
+import { useState } from 'react';
+import { flowRight as compose } from 'lodash';
+import { graphql } from '@apollo/client/react/hoc';
 
-function AddBook() {
+
+function AddBook(props) {
+
+    const [name, setName] = useState('');
+    const [genre, setGenre] = useState('');
+    const [authorId, setAuthorId] = useState('');
+    const [addBook] = useMutation(addBookMutation, {
+        refetchQueries: [ { query: getBooksQuery } ]
+    });
+
     const { loading, error, data } = useQuery(getAuthorsQuery);
     if (loading) return <p>Loading authors...</p>;
     if (error) return <p>Error :(</p>;
     
+    const submitForm= (e) => {
+        e.preventDefault();
+        console.log('form submitted')
+        addBook({
+            variables: {name: name, genre: genre, authorId: authorId}
+        });
+    }
+    
     return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={e => { submitForm(e) }}>
+
             <div className="field">
                 <label>Book name:</label>
-                <input type="text"/>
+                <input type="text" name="name" onChange={e => setName(e.target.value)}/>
             </div>
         
             <div className="field">
                 <label>Genre:</label>
-                <input type="text"/>
+                <input type="text" name="genre" onChange={e => setGenre(e.target.value)}/>
             </div>
         
             <div className="field">
                 <label>Author:</label>
-                <select>
+                <select name="authorId" onChange={e => setAuthorId(e.target.value)}>
                     {data.authors.map(({ name, id }) => (
-                        <option key={id}>{name}</option>
+                        <option key={id} value={id}>{name}</option>
                     ))};
-                    <option>NO ONE</option>
                 </select>
             </div>
         
-            <button></button>
+            <button> ADD+</button>
+
         </form>
     )
 }
 
-export default AddBook;
+export default compose(
+    graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+    graphql(addBookMutation, {name: "addBookMutation"}),
+)(AddBook);
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import { useQuery } from "@apollo/client";
-import getBooksQuery from './graphql/getbooks'
+import { getBooksQuery } from '../graphql/queries'
+import BookDetails from './BookDetails';
+import { useState } from 'react';
 
 
 function BookList() {
-    const { loading, error, data } = useQuery(getBooksQuery);
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
-    
-    console.log(data)
+  const [selectedID, setSelectedID] = useState(null);
+  const { loading, error, data } = useQuery(getBooksQuery);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
-    return data.books.map(({ name, genre, id }) => (
-        <div key={id}>
-          <ul id="book-list">
-            <li>{name} - ({genre})</li>
-          </ul>
-        </div>
-    ));
+  return (
+    <div>
+      <ul id="book-list" >
+        {data.books.map(({ name, genre, id }) => (
+          <li key={id} onClick={(e) => { setSelectedID(id) }}>
+            {name} - ({genre})
+          </li>
+        ))}
+      </ul>
+      <BookDetails bookId={ selectedID }/>
+    </div>
+  )
 
 }
 
